0f5efef7867cbe360e698fa9401664c70fe63558
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2021-07-10 12:19:39 +0300

    add window specific transformations

--- spanda/sql/functions.py
+++ spanda/sql/functions.py
@@ -1,6 +1,8 @@
 import math
 
 # helper functions
+import warnings
+
 import pandas as pd
 
 
@@ -57,13 +59,27 @@ def last(col):
     return AggColumn(name="LAST", orig_col=col, op=lambda x: x.iloc[-1])
 
 
+def count(col):
+    if col != "*":
+        warnings.warn("count(col) ignores the column it gets. Use count('*') instead to avoid this message")
+    return AggColumn(name="COUNT", orig_col=col, op=len)
+
+
+def countDistinct(col):
+    # TODO: this functions needs to be fixed
+    return AggColumn(name="COUNT DISTINCT", orig_col=col, op=lambda x: len(set(x[Column.getName(col)])))
+
+
 # window-only functions
 def lead(col, count=1):
-    raise NotImplementedError
+    return WindowTransformationColumn(name=f"LEAD {count}", orig_col=col,
+                                      op=lambda col, grp, pos: col.loc[grp[pos+count]] if pos+count < len(grp) else None)
 
 
 def lag(col, count=1):
-    raise NotImplementedError
+    return WindowTransformationColumn(name=f"LAG {count}", orig_col=col,
+                                      op=lambda col, grp, pos: col.loc[grp[pos-count]] if pos-count >= 0 else None)
+
 
 
 # classes
@@ -109,14 +125,20 @@ class Column:
 
     def alias(self, name):
         return Column._transformColumn(name, self.op)
-    
+
+    def between(self, start, end):
+        return (self >= start) & (self <= end)
+
     def isin(self, values):
         return self._simpleBinaryTransformColumn('IN', lambda x, y: x.isin(y), values, is_other_col=False)
 
     # operators
     def __eq__(self, other):
         return self._simpleBinaryTransformColumn('==', lambda x, y: x == y, other)
-    
+
+    def __ne__(self, other):
+        return self._simpleBinaryTransformColumn('!=', lambda x, y: x != y, other)
+
     def __gt__(self, other):
         return self._simpleBinaryTransformColumn('>', lambda x, y: x > y, other)
     
@@ -142,49 +164,70 @@ class Column:
         return self._simpleBinaryTransformColumn('-', lambda x, y: x - y, other)
 
     def __and__(self, other):
-        return self._simpleBinaryTransformColumn(' AND ', lambda x, y: x and y, other)
+        return self._simpleBinaryTransformColumn(' AND ', lambda x, y: x & y, other)
 
     def __or__(self, other):
-        return self._simpleBinaryTransformColumn(' OR ', lambda x, y: x or y, other)
+        return self._simpleBinaryTransformColumn(' OR ', lambda x, y: x | y, other)
 
     def __neg__(self, other): 
         return self._simpleUnaryTransformColumn('-', lambda x: -x)
 
-    def __invert__(self, other): 
-        return self._simpleUnaryTransformColumn('NOT ', lambda x: not x)
+    def __invert__(self):
+        return self._simpleUnaryTransformColumn('NOT ', lambda x: ~x)
 
 
 class AggColumn:
     def __init__(self, name, orig_col, op):
-        self.name = name
-        self.orig_col = orig_col
-        self.op = op
+        self._name = name
+        self._orig_col = orig_col
+        self._op = op
 
     def alias(self, name):
         # TODO: might want to clone orig_col and op (maybe also in Column)
-        return AggColumn(name=name, orig_col=self.orig_col, op=self.op)
+        return AggColumn(name=name, orig_col=self._orig_col, op=self._op)
 
     @staticmethod
     def getName(agg_col):
-        return f"{agg_col.name} ({Column.getName(agg_col.orig_col)})"
+        return f"{agg_col._name} ({Column.getName(agg_col._orig_col)})"
 
     @staticmethod
     def _apply(agg_col, df):
-        return agg_col.op(Column._apply(agg_col.orig_col, df))
+        return agg_col._op(Column._apply(agg_col._orig_col, df))
 
     def over(self, window_spec):
         def f(df):
-            inputs = Column._apply(self.orig_col, df)
+            inputs = Column._apply(self._orig_col, df)
             # in the following, row2grp represents the representative group of each row, while grp2rows is a dictionary
             # of all rows that the group aggregates over. possibly not intuitive but group may include rows that are
             # not represented by this group (for example if we apply lead(...) we aggregate over the next row which is
             # not represented by the current group)
             row2grp, grp2rows = window_spec._get_group_data(df)
-            print((row2grp, grp2rows))
-            grp_agg_val = {grp: self.op(inputs.loc[vals]) for grp, vals in grp2rows.items()}
+            grp_agg_val = {grp: self._op(inputs.loc[vals]) for grp, vals in grp2rows.items()}
             data = {row_idx: grp_agg_val[grp_name] for row_idx, grp_name in row2grp.items()}
             return pd.Series(data=data, index=inputs.index)
-        col = Column(name=f"{AggColumn.getName(self)} OVER {window_spec._name}")
+        col = Column(name=f"{AggColumn.getName(self)} OVER ({window_spec._name})")
+        col.setOp(f)
+        return col
+
+
+class WindowTransformationColumn(AggColumn):
+    def __init(self, name, orig_col, op):
+        self._name = name
+        self._orig_col = orig_col
+        self._op = op
+
+    @staticmethod
+    def _apply(agg_col, df):
+        assert False, f"cannot aggregate grouped data using `{agg_col._name}`. can be used only over windows."
+
+    def over(self, window_spec):
+        def f(df):
+            inputs = Column._apply(self._orig_col, df)
+            row2grp, grp2rows = window_spec._get_group_data(df)
+            data = {row_idx: self._op(inputs, grp2rows[grp_name], grp2rows[grp_name].index(row_idx))
+                    for row_idx, grp_name in row2grp.items()}
+            return pd.Series(data=data, index=inputs.index)
+        col = Column(name=f"{AggColumn.getName(self)} OVER ({window_spec._name})")
         col.setOp(f)
         return col
 
